055e324e377a16c3dff6bc7da7eb1c77145f644c
slaaiblaar <u19043512@tuks.co.za>
2024-08-12 21:36:28 +0200

    Represent CFG as XML, initialise project skeleton, add basic cmake

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # RecSPL-Compiler
 Semester project for COS341
+
+Note (12/08/24): 
+ - RecSPL production in CFG.xml
+ - main.cpp simply reads CFG.cml and prints the productions to terminal
+ - To run project:
+```
+project-root:$ cmake .
+project-root:$ make
+project-root:$ ./bin/app
+```
---
+++ src/app/src/main.cpp
@@ -0,0 +1,37 @@
+// #include "include/pugixml/pugixml.hpp"
+// #include <pugixml/pugixml.hpp>
+#include "pugixml.hpp"
+#include <iostream>
+int main()
+{
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("./CFG.xml");
+    if (!result)
+    {
+        std::cout << "Load result: " << result.description() << std::endl;
+        return -1;
+    }
+    pugi::xml_node lhs = doc.child("CFG").first_child();
+    do {
+        std::cout << std::endl << lhs.name() << ":";
+        pugi::xml_node production = lhs.first_child();
+        do {
+            std::cout << "\n\trhs:";
+            pugi::xml_node rhs = production.first_child();
+            do {
+                if (rhs.attribute("terminal").as_bool() == false) {
+                    std::cout << " " << rhs.name();
+                    continue;
+                }
+                else {
+                    std::cout << " " << rhs.first_child().child_value();
+                }
+                // std::cout << " " << rhs.name() << "(" << rhs.attribute("terminal").value() << ")";
+            } while ((rhs = rhs.next_sibling()) != pugi::xml_node());
+        } while ((production = production.next_sibling()) != pugi::xml_node());
+
+    } while ((lhs = lhs.next_sibling()) != pugi::xml_node());
+
+
+    return 0;
+}
