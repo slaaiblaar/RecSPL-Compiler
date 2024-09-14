a08db11d5d6349b2932c8be77add71d10b3e377f
SelenatorXo <thashlene@gmail.com>
2024-09-14 23:13:14 +0200

    Function to calculate FIRST sets

--- src/parser/include/parser.hpp
+++ src/parser/include/parser.hpp
@@ -3,18 +3,23 @@
 #include "automata.hpp"
 #include "pugixml.hpp"
 #include <unordered_map> // hashmap
+#include <set>
 class Parser
 {
 public:
     Parser();
     void get_nullable();
-    // void generate_first_sets();
+    void generate_first_sets();
     // void generate_follow_sets();
     // void read_token_stream(std::string input);
 
 private:
     pugi::xml_document cfg_doc;
+    std::unordered_map<std::string, std::set<std::string>> first;  // FIRST sets
+
     // pugi::xml_parse_result token_stream;
     std::unordered_map<std::string, bool> nullable;
+    
+    bool is_terminal(const std::string& symbol); // helper function
 };
 #endif
--- src/parser/src/parser.cpp
+++ src/parser/src/parser.cpp
@@ -15,6 +15,7 @@ Parser::Parser()
         return;
     }
     this->get_nullable();
+    this->generate_first_sets();
 }
 
 void Parser::get_nullable()
@@ -88,4 +89,112 @@ void Parser::get_nullable()
     // {
     //     std::cout << pair.first << ": " << pair.second << std::endl;
     // }
+}
+
+bool Parser::is_terminal(const std::string& symbol)
+{
+    // in this context, terminals are symbols that do not have production rules defined
+    // we'll assume a terminal is a symbol not found in the CFG's non-terminals
+    pugi::xml_node productions = this->cfg_doc.child("CFG").child("PRODUCTIONRULES");
+    pugi::xml_node non_terminal = productions.first_child();
+
+    // check if the symbol is a non-terminal
+    do
+    {
+        if (symbol == non_terminal.name())
+        {
+            return false;  // it's a non-terminal
+        }
+    } while ((non_terminal = non_terminal.next_sibling()) != pugi::xml_node());
+
+    return true;  // if we didn't find it in non-terminals, it's a terminal
+}
+
+void Parser::generate_first_sets()
+{
+    std::cout << "Generating FIRST sets\n";
+    pugi::xml_node productions = this->cfg_doc.child("CFG").child("PRODUCTIONRULES");
+    pugi::xml_node non_terminal = productions.first_child();
+
+    // Initialize FIRST sets
+    do
+    {
+        this->first[non_terminal.name()] = std::set<std::string>();  // Using a set to avoid duplicates
+    } while ((non_terminal = non_terminal.next_sibling()) != pugi::xml_node());
+
+    bool changed = true;
+    while (changed)
+    {
+        non_terminal = productions.first_child();
+        changed = false;
+        do
+        {
+            // Iterate through each production for the non-terminal
+            pugi::xpath_node_set non_terminal_rhs_set = non_terminal.select_nodes("production");
+            for (pugi::xpath_node_set::const_iterator rhs_it = non_terminal_rhs_set.begin(); rhs_it != non_terminal_rhs_set.end(); ++rhs_it)
+            {
+                pugi::xml_node rhs = (*rhs_it).node();
+                pugi::xml_node symbol = rhs.first_child();
+
+                // Iterate through symbols in RHS until a terminal or non-nullable non-terminal is found
+                while (symbol != pugi::xml_node())
+                {
+                    std::string symbol_name = symbol.name();
+
+                    // If the symbol is a terminal, add it to FIRST set
+                    if (this->is_terminal(symbol_name))
+                    {
+                        if (this->first[non_terminal.name()].insert(symbol_name).second)  // If newly added
+                        {
+                            changed = true;
+                        }
+                        break;
+                    }
+                    else  // It's a non-terminal
+                    {
+                        // Add all non-epsilon FIRST symbols of the non-terminal to the current non-terminal
+                        for (const std::string &first_symbol : this->first[symbol_name])
+                        {
+                            if (first_symbol != "EPSILON")
+                            {
+                                if (this->first[non_terminal.name()].insert(first_symbol).second)
+                                {
+                                    changed = true;
+                                }
+                            }
+                        }
+
+                        // If the non-terminal is not nullable, stop the loop
+                        if (!this->nullable[symbol_name])
+                        {
+                            break;
+                        }
+                    }
+
+                    symbol = symbol.next_sibling();
+                }
+
+                // If all symbols in RHS are nullable, add EPSILON to the FIRST set
+                if (symbol == pugi::xml_node())
+                {
+                    if (this->first[non_terminal.name()].insert("EPSILON").second)
+                    {
+                        changed = true;
+                    }
+                }
+            }
+        } while ((non_terminal = non_terminal.next_sibling()) != pugi::xml_node());
+    }
+
+    // Optional: Print out the FIRST sets for debugging
+    std::cout << "FIRST Sets:\n";
+    for (const auto &pair : this->first)
+    {
+        std::cout << pair.first << ": { ";
+        for (const std::string &s : pair.second)
+        {
+            std::cout << s << " ";
+        }
+        std::cout << "}\n";
+    }
 }
